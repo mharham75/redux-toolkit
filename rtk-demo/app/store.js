e28a6e7e6b3c2f3902bec4d8079b35b6082b1687
Md Arham <md.arham@globallogic.com>
2022-07-25 21:59:53 +0530

    added async actions

--- rtk-demo/app/store.js
+++ rtk-demo/app/store.js
@@ -1,16 +1,18 @@
 const configureStore = require('@reduxjs/toolkit').configureStore
-const reduxLogger = require('redux-logger')
+//const reduxLogger = require('redux-logger')
 const cakeReducer = require('../features/cake/cakeSlice')
 const chocolateReducer = require('../features/chocolate/chocolateSlice')
+const userReducer = require('../features/user/userSlice')
 
-const logger = reduxLogger.createLogger()
+//const logger = reduxLogger.createLogger()
 
 const store = configureStore({
     reducer: {
         cake: cakeReducer,
         chocolate: chocolateReducer,
+        user: userReducer,
     },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+    //middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 })
 
 module.exports = store
--- rtk-demo/index.js
+++ rtk-demo/index.js
@@ -1,9 +1,14 @@
 const store = require('./app/store')
 const cakeActions = require('./features/cake/cakeSlice').cakeActions
 const chocolateActions = require('./features/chocolate/chocolateSlice').chocolateActions
+const fetchUsers = require('./features/user/userSlice').fetchUsers
 
 console.log('Initial state ' , store.getState());
-const unsubscribe = store.subscribe( () => {})
+const unsubscribe = store.subscribe( () => { console.log("Updated state => ",store.getState())})
+
+store.dispatch(fetchUsers())
+
+/*
 store.dispatch(cakeActions.ordered())
 store.dispatch(cakeActions.ordered())
 store.dispatch(cakeActions.ordered())
@@ -14,3 +19,4 @@ store.dispatch(chocolateActions.ordered())
 store.dispatch(chocolateActions.restocked(2))
 
 unsubscribe()
+*/
